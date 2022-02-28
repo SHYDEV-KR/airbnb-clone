da2a618ca9ab5d2f09579b1c127a1ef6efd4898a
paulyoon28 <ekdmbzzang@naver.com>
2022-02-28 15:17:36 +0900

    User App

--- users/admin.py
+++ users/admin.py
@@ -1,7 +1,22 @@
+from dataclasses import field
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
 from . import models
 
 # Register your models here.
 @admin.register(models.User)
-class CustomUserAdmin(admin.ModelAdmin):
-    pass
+class CustomUserAdmin(UserAdmin):
+
+    """ Custom User Admin """
+    fieldsets = UserAdmin.fieldsets + (
+        (
+            "Custom Profile",
+            {
+                "fields": (
+                    "avatar",
+                    "gender",
+                    "bio",
+                )
+            }
+        ),
+    )
